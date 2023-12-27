a83160c88cbffa24cd53f31d0f78016a381e8386
Rioni <nrosa01@ucm.es>
2023-12-27 20:12:28 +0100

    Readded pwa plgin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import litCss from 'vite-plugin-lit-css'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const base = process.env.VITE_BASE ?? "/"
 
 export default defineConfig({
-  plugins: [litCss()],
+  plugins: [litCss(), VitePWA({ registerType: 'autoUpdate', devOptions: { enabled: true } })],
   base: base
 })
