3a10dfa0ba13a818b95d5c7e8815a868689ecab7
Rioni <nrosa01@ucm.es>
2023-12-27 20:04:14 +0100

    Project init

---
+++ src/my-element.ts
@@ -0,0 +1,39 @@
+import { html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import { TailwindElement } from './core/tailwind'
+
+@customElement('my-element')
+export class MyElement extends TailwindElement {
+  
+  @property({ type: Array }) todos: string[] = [];
+
+  @property({ type: String }) todoText:string = '';
+
+  connectedCallback() {
+    super.connectedCallback();
+    const existingTodos = localStorage.getItem('todos');
+    this.todos = JSON.parse(existingTodos as string) || [];
+  }
+
+
+  addTodo(event: Event) {
+    event.preventDefault();
+    console.log(this.todoText, event);
+    this.todos = [...this.todos, this.todoText];
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  render() {
+    return html`
+
+    <ul>
+      ${this.todos.map(todo => html`<li>${todo}</li>`)}
+    </ul>
+
+      <form @submit="${this.addTodo}">
+        <input type="text" .value="${this.todoText}" @change=${(e: any) => this.todoText = e.target.value} />
+        <input type="submit">
+      </form>
+    `;
+  }
+}
