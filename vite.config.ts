ad56db3b120815e328413acdbe7248889cf0ff30
Rioni <nrosa01@ucm.es>
2023-12-27 20:10:11 +0100

    Removed pwa plugin to test build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import litCss from 'vite-plugin-lit-css'
-import { VitePWA } from 'vite-plugin-pwa'
 
 const base = process.env.VITE_BASE ?? "/"
 
 export default defineConfig({
-  plugins: [litCss(), VitePWA({ registerType: 'autoUpdate', devOptions: { enabled: true } })],
+  plugins: [litCss()],
   base: base
 })
